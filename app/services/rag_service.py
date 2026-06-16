188a058903dc352bedb8acdf1426468f48935006
Alifah Zuhrah <alifah.zuhrah@gmail.com>
2026-06-16 20:39:00 +0700

    fix s3 aws storage

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,10 +1,29 @@
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms import Ollama
+import os
+import boto3
 
+s3 = boto3.client("s3")
+
+bucket = "fake-news-uas"
+prefix = "faiss_index"
 llm = Ollama(model="llama3.2:3b")
-
 embedding = OllamaEmbeddings(model="llama3")
+
+local_dir = "/home/ubuntu/fake-news-chatbot/faiss_index"
+os.makedirs(local_dir, exist_ok=True)
+s3.download_file(
+    bucket,
+    f"{prefix}/index.faiss",
+    f"{local_dir}/index.faiss"
+)
+
+s3.download_file(
+    bucket,
+    f"{prefix}/index.pkl",
+    f"{local_dir}/index.pkl"
+)
 
 vectorstore = FAISS.load_local(
     "faiss_index",
